011da845073d3f41e85c7e0ba09e243842d760e2
Anna <Anna.khizhnyakova@gmail.com>
2022-06-14 08:38:12 +0300

    Задача 23
    Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
    
    3 -> 1, 8, 27
    5 -> 1, 8, 27, 64, 125

---
+++ Lec3_Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Enter the number: ");
+int num = int.Parse(ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    WriteLine(Math.Pow(i, 3));
+}
