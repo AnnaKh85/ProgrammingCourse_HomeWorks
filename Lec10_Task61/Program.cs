9bf89e3b74c12f1124e2c2832f81c07d8051c4b7
Anna <Anna.khizhnyakova@gmail.com>
2022-07-09 06:04:47 +0300

    Задача 61 Показать треугольник Паскаля. *Сделать вывод в виде равнобедренного треугольника.

---
+++ Lec10_Task61/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Задача 61: Показать треугольник Паскаля. 
+Сделать вывод в виде равнобедренного треугольника.
+ */
+
+using System;
+using static System.Console;
+
+Clear();
+
+int i, c;
+Write("Enter the number of rows: ");
+string s = ReadLine();
+int n = Convert.ToInt32(s);
+
+int factorial(int n)
+{
+    int i, x = 1;
+    for (i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+for (i = 0; i < n; i++)
+{
+    for (c = 0; c <= (n - i); c++)
+    {
+        Write(" ");
+    }
+    for (c = 0; c <= i; c++)
+    {
+        Write(" "); 
+        Write(factorial(i) / (factorial(c) * factorial(i - c))); 
+    }
+    WriteLine();
+}
